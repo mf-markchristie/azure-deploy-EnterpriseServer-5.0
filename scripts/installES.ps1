b6834ee47d5f2966ebc04a768db113f1a9f703d2
mf-markchristie <48920686+mf-markchristie@users.noreply.github.com>
2019-08-29 15:12:10 +0100

    Installation scripts and separate network configuration into rdgDeploy.json

---
+++ scripts/installES.ps1
@@ -0,0 +1,82 @@
+<#
+ .SYNOPSIS
+    Machine setup for Marketplace deployment
+
+ .DESCRIPTION
+    Machine setup for Marketplace deployment
+
+ .PARAMETER installerName
+    The name of the installer
+
+ .PARAMETER licenseKey
+    Optional, the license key to be used
+#>
+param(
+    [string]
+    $licenseKey
+)
+
+Write-Host "Creating temporary working directory."
+mkdir -path "c:\tmp"
+Set-Location "c:\tmp"
+
+$installerExeName = "es.exe"
+$updateExeName = "es_update.exe"
+$installerLocation ="https://mfenterprisestorage.blob.core.windows.net/enterpriseserverdeploy"
+
+Write-Host "Downloading installer $installerExeName"
+
+Import-Module BitsTransfer
+Start-BitsTransfer -Source "$installerLocation/$installerExeName" -Destination ("./" + $installerExeName)
+if(!$?) {
+    Write-Error "Failed to download installer"
+    exit 500
+}
+
+Write-Host "Successfully downloaded installer, starting installation."
+
+Start-Process -FilePath $installerExeName -ArgumentList "/q /log c:\tmp\log.txt" -Wait
+
+Write-Host "Checking installation."
+
+if (!(Select-String -Path ".\log.txt" -Pattern "Exit Code: 0x0")) {
+    Write-Error "Install failed - error messages in log.txt"
+    Select-String -Path ".\log.txt" -Pattern "Exit Code:"
+    exit 500
+}
+
+Write-Host "Successfully installed product"
+
+Start-BitsTransfer -Source "$installerLocation/$updateExeName" -Destination ("./" + $updateExeName)
+if($?) {
+    Write-Host "Installing update file."
+    Start-Process -FilePath $updateExeName -ArgumentList "/q /log c:\tmp\log.txt" -Wait
+    if (!(Select-String -Path ".\log.txt" -Pattern "Exit Code: 0x0")) {
+        Write-Error "Install failed - error messages in log.txt"
+        Select-String -Path ".\log.txt" -Pattern "Exit Code:"
+        exit 500
+    }
+}
+
+if ($licenseKey -ne "") {
+    Write-Host "Installing license."
+    Start-Process -FilePath "C:\Program Files (x86)\Common Files\SafeNet Sentinel\Sentinel RMS License Manager\WinNT\cesadmintool" -ArgumentList "-term activate $licenseKey" -Wait
+}
+else {
+    Write-Warning "No license key provided during installation, a license will need to be added before you can use the product."
+}
+
+
+Write-Host "Setting up Data Disk"
+Get-Disk | Where-Object PartitionStyle -Eq "RAW" | Initialize-Disk -PartitionStyle MBR
+New-Partition -DiskNumber 2 -UseMaximumSize -DriveLetter f
+Format-Volume -DriveLetter F -FileSystem NTFS -Confirm:$false
+Get-Volume -DriveLetter F | Set-Volume -NewFileSystemLabel "Data Drive"
+
+
+Write-Host "Cleaning up temporary working directory."
+Set-Location "C:\"
+Remove-Item "C:\tmp" -Force -Recurse
+
+Write-Host "Stopping MFDS service for repository imports"
+Stop-Service -Name "MF_CCITCP2"
