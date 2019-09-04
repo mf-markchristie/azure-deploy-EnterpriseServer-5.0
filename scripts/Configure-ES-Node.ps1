5aea1492e1b3a238ea3b2cc407daff21e679dd22
mf-markchristie <48920686+mf-markchristie@users.noreply.github.com>
2019-09-04 09:43:22 +0100

    - Dependency corrections - Add all calls to getLoopName - Reset NIC for ES nodes after DC setup

--- scripts/Configure-ES-Node.ps1
+++ scripts/Configure-ES-Node.ps1
@@ -1,34 +1,45 @@
 param(
-    [Parameter(Mandatory)]
-    [string]DomainNetBIOSName
+    [Parameter(Mandatory = $True)]
+    [string]$DomainNetBIOSName,
 
-    [Parameter(Mandatory)]
-    [string]ServiceUser
+    [Parameter(Mandatory = $True)]
+    [string]$ServiceUser,
 
-    [Parameter(Mandatory)]
-    [System.Security.SecureString]ServicePassword
+    [Parameter(Mandatory = $True)]
+    [System.Security.SecureString]$ServicePassword,
 
-    [Parameter(Mandatory)]
-    [string]DemoUser
+    [Parameter(Mandatory = $True)]
+    [string]$DemoUser,
 
-    [Parameter(Mandatory)]
-    [System.Security.SecureString]DemoPassword
+    [Parameter(Mandatory = $True)]
+    [System.Security.SecureString]$DemoPassword
 )
 
-Write-Host "Creating temporary working directory."
-mkdir -path "c:\tmp"
-Set-Location "c:\tmp"
-
 Write-Host "Configuring Service Account"
-$PSScriptRoot/Configure-UserLogonPrivileges.ps1 -Username "${DomainNetBIOSName}\${ServiceUser}" -PrivilegeName "SeServiceLogonRight" -Status Grant
-$PSScriptRoot/Configure-UserLogonPrivileges.ps1 -Username "${DomainNetBIOSName}\${ServiceUser}" -PrivilegeName "SeBatchLogonRight" -Status Grant
+$cmd = "$PSScriptRoot/Configure-UserLogonPrivileges.ps1"
+$args = @()
+$args += ("-Username", "${DomainNetBIOSName}\${ServiceUser}")
+$args += ("-PrivilegeName", 'SeServiceLogonRight')
+$args += ("-Status", 'Grant')
+Write-Host "$cmd $args"
+Invoke-Expression "$cmd $args"
+
+$args = @()
+$args += ("-Username", "${DomainNetBIOSName}\${ServiceUser}")
+$args += ("-PrivilegeName", 'SeBatchLogonRight')
+$args += ("-Status", 'Grant')
+Invoke-Expression "$cmd $args"
 Stop-Service -Name "MF_CCITCP2"
 $ServiceCredentials = New-Object System.Management.Automation.PSCredential -ArgumentList "${DomainNetBIOSName}\${ServiceUser}", $ServicePassword
 Set-Service -Name "MF_CCITCP2" -Credential $ServiceCredentials
 Remove-Service -Name "ESCWA"
 
 Write-Host "Configuring Demo Account"
-$PSScriptRoot/Configure-UserLogonPrivileges.ps1 -Username "${DomainNetBIOSName}\${DemoUser}" -PrivilegeName "SeRemoteInteractiveLogonRight" -Status Grant
+$args = @()
+$args += ("-Username", "${DomainNetBIOSName}\${DemoUser}")
+$args += ("-PrivilegeName", 'SeRemoteInteractiveLogonRight')
+$args += ("-Status", 'Grant')
+Invoke-Expression "$cmd $args"
 
 
 Write-Host "Starting Directory Server"
