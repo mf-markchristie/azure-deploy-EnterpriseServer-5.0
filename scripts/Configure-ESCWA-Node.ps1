a19df8f20b951d90d97e71a9d80966339d026b3b
mf-markchristie <48920686+mf-markchristie@users.noreply.github.com>
2019-09-11 13:56:10 +0100

    Finalize escwa node configuration

--- scripts/Configure-ESCWA-Node.ps1
+++ scripts/Configure-ESCWA-Node.ps1
@@ -6,7 +6,13 @@ param(
     [string]$ServiceUser,
 
     [Parameter(Mandatory = $True)]
-    [string]$ServicePassword
+    [string]$ServicePassword,
+
+    [Parameter(Mandatory = $True)]
+    [int]$ESCount,
+
+    [Parameter(Mandatory = $True)]
+    [string]$clusterPrefix
 )
 $ErrorActionPreference = "Stop"
 
@@ -29,9 +35,19 @@ Stop-Service -Name "ESCWA"
 $Account="${DomainNetBIOSName}\${ServiceUser}"
 $Service=gwmi win32_service -filter "Name='ESCWA'"
 $Service.change($null,$null,$null,$null,$null,$false,$Account,$ServicePassword,$null,$null,$null)
+
+
+$Directory = "C:\ProgramData\Micro Focus\Enterprise Developer\ESCWA"
+$Inherit = [system.security.accesscontrol.InheritanceFlags]"ContainerInherit, ObjectInherit"
+$Propagation = [system.security.accesscontrol.PropagationFlags]"None"
+$Acl = Get-Acl $directory
+$Accessrule = New-Object system.security.AccessControl.FileSystemAccessRule($Account,"FullControl", $inherit, $propagation, "Allow")
+$Acl.AddAccessRule($Accessrule)
+Set-Acl -aclobject $Acl $Directory
 Start-Service -Name "ESCWA"
 
 Write-Host "Deleting MFDS Service"
+Stop-Service -Name "MF_CCITCP2"
 $Service=gwmi win32_service -filter "Name='MF_CCITCP2'"
 $Service.delete()
 
@@ -40,7 +56,7 @@ function addDS {
     Param
     (
         [Parameter(Mandatory = $true)]
-        [string] $Host,
+        [string] $HostName,
         [Parameter(Mandatory = $true)]
         [string] $Port,
         [Parameter(Mandatory = $true)]
@@ -48,11 +64,11 @@ function addDS {
     )
 
     $JMessage = '
-{
-    \"MfdsHost\": \"' + $Host + '\",
-    \"MfdsIdentifier\": \"' + $Name + '\",
-    \"MfdsPort\": ' + $Port + '
-}'
+    {
+        \"MfdsHost\": \"' + $HostName + '\",
+        \"MfdsIdentifier\": \"' + $Name + '\",
+        \"MfdsPort\": \"' + $Port + '\"
+    }'
 
     $RequestURL = 'http://localhost:10004/server/v1/config/mfds'
     $Origin = 'Origin: http://localhost:10004'
@@ -61,13 +77,23 @@ function addDS {
 }
 
 Write-Host "Configuring ESCWA"
-$JMessage = '{ \"mfUser\": \"SYSAD\", \"mfPassword\": \"SYSAD\" }'
+$JMessage = '{ \"mfUser\": \"\", \"mfPassword\": \"\" }'
 
 $RequestURL = 'http://localhost:10004/logon'
 $Origin = 'Origin: http://localhost:10004'
 
 curl.exe -sX POST  $RequestURL -H 'accept: application/json' -H 'X-Requested-With: AgileDev' -H 'Content-Type: application/json' -H $Origin -d $Jmessage --cookie-jar cookie.txt | Out-Null
-addDS -Host "mfes-es01" -Name "mfes-es01" -Port 86
-addDS -Host "mfes-es01" -Name "mfes-es02" -Port 86
+
+$RequestURL = 'http://localhost:10004/server/v1/config/mfds'
+$mfdsObj = curl.exe -sX GET $RequestURL -H 'accept: application/json' -H 'X-Requested-With: AgileDev' -H 'Content-Type: application/json' -H $Origin --cookie cookie.txt | ConvertFrom-Json
+$Uid=$mfdsObj[0].Uid
+$RequestURL = "http://localhost:10004/server/v1/config/mfds/$Uid"
+curl.exe -sX DELETE $RequestURL -H 'accept: application/json' -H 'X-Requested-With: AgileDev' -H 'Content-Type: application/json' -H $Origin --cookie cookie.txt | Out-Null
+
+addDS -HostName "$clusterPrefix-es01" -Name "$clusterPrefix-es01" -Port "86"
+if ($ESCount -eq 2) {
+    addDS -HostName "$clusterPrefix-es02" -Name "$clusterPrefix-es02" -Port "86"
+}
+addDs -HostName "$clusterPrefix-fs" -Name "$clusterPrefix-fs" -Port "86"
 
 Set-NetFirewallProfile -Profile Domain -Enabled False
