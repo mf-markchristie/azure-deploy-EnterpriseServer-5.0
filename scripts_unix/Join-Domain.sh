8660268461bfff159f479f7fefce02468771af39
mf-markchristie <48920686+mf-markchristie@users.noreply.github.com>
2019-11-25 13:26:35 +0000

    New files

---
+++ scripts_unix/Join-Domain.sh
@@ -0,0 +1,33 @@
+#! /bin/bash -e
+if [ "$#" -ne 3 ]
+then
+  echo "Not Enough Arguments supplied."
+  echo "JoinTo-Domain-Linux.sh <Join_account> <Domain> <Password>"
+  exit 1
+fi
+
+Join_account=$1
+Domain=$2
+Password=$3
+
+yum install realmd oddjob oddjob-mkhomedir sssd samba-common samba-common-tools -y
+
+# Join the domain
+echo $Password | realm join -v -U $Join_account $Domain --install=/
+
+# Allow domain users to logon via passwords
+sed -i '/PasswordAuthentication/s/no.*/yes/' /etc/ssh/sshd_config
+systemctl restart sshd.service
+
+realm permit Admin@$Domain
+
+#Give domain admins sudo privledges
+echo "%AWS\ Delegated\ Administrators@$Domain ALL=(ALL:ALL) ALL" | EDITOR='tee -a' visudo
+
+if [ $? -eq 0 ]; then
+    echo "JoinTo-Domain-Linux has passed"
+    exit 0
+else
+    echo "JoinTo-Domain-Linux has FAILED"
+    exit 1
+fi
